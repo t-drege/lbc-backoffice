74295c396e15a290503556762d7831bdcc75b3e2
drege <thomasdrege1@gmail.com>
2021-12-16 10:29:21 +0100

    first commit config

---
+++ nuxt.config.js
@@ -0,0 +1,67 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'lbc-backoffice',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''},
+      {name: 'format-detection', content: 'telephone=no'}
+    ],
+    link: [
+      {rel: 'icon', type: 'image/x-icon', href: '/lbc-icon.ico'}
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    '@nuxtjs/fontawesome',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/router-extras'
+  ],
+  fontawesome: {
+    icons: {
+      solid: true,
+    }
+  },
+  googleFonts: {
+    families: {
+      // a simple name
+      Corinthia: true,
+      Roboto:true,
+      Lobster:true,
+
+      // a name with spaces
+      'Josefin+Sans': true,
+
+      // specific font weights
+      Lato: [100, 300],
+    }
+  },
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+  ],
+
+  router : {
+    middleware: []
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {}
+}
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <ContentView/>
+</template>
+<script lang="ts">
+
+  import Vue from 'vue'
+  import ContentView from "~/components/Body/ContentView.vue";
+
+  export default Vue.extend({
+    name: 'IndexPage',
+    components: {ContentView},
+    layout: ({isMobile}) => isMobile ? 'mobile' : 'default',
+  })
+</script>
+
+<style>
+  body {
+    background-color: #F5F5F5;
+    height: 100vh;
+  }
+</style>
